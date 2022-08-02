eaff0e4880e129e6423e794db996e85cbcc192e7
Kasey Jones <kaseyjones49@gmail.com>
2022-08-02 15:35:59 -0500

    Created urls.py and serializer.py files but am going through now and setting them up

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Super
+from super_types.models import SuperType
+
 
 # Create your views here.
+@api_view(['GET'])
+def supers_list (request):
+    supers = Super.objects.all()
+    return 
