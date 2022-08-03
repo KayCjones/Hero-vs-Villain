4ddf0f1934d9d7a899a3ef3805270dd1d172de31
Kasey Jones <kaseyjones49@gmail.com>
2022-08-03 14:36:21 -0500

    Successfully created a conditional under the supers_list function that allows params to be passed in to list only the type passed in

--- supers/views.py
+++ supers/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from super_types.serializer import SuperTypeSerializer
 from .models import Super
 from super_types.models import SuperType
 from .serializer import SuperSerializer
-
 
 
 # Create your views here.
@@ -13,7 +13,14 @@ from .serializer import SuperSerializer
 @api_view(['GET', 'POST'])
 def supers_list (request):
 
-    if request.method == 'GET':
+    supers_param = request.query_params.get('type')
+    supers = Super.objects.all()
+    if request.method == 'GET' and 'type' in request.GET:
+        supers = supers.filter(super_type__type=supers_param)
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'GET':
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -32,11 +39,13 @@ def supers_detail(request, pk):
         if request.method == 'GET':
             serializer = SuperSerializer(super)
             return Response(serializer.data, status=status.HTTP_200_OK)
+
         elif request.method == 'PUT':
             serializer = SuperSerializer(super, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+
         elif request.method == 'DELETE':
             super.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
